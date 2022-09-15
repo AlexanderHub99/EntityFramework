f44091c926aa6ff54a0e755fe04b8aefbaeeb376
Александр Середин <agames1448@gmail.com>
2022-09-15 23:28:38 +0300

    [refactor]

--- GeneratingPropertyAndColumnValues/Program.cs
+++ GeneratingPropertyAndColumnValues/Program.cs
@@ -9,8 +9,10 @@ using (ApplicationContext db = new())
 {
     User user = new User { Name = "Tom" };
     Console.WriteLine($"Id перед добавлением в контекст {user.Id}");    // Id = 0
+
     db.Users.Add(user);
     db.SaveChanges();
+
     Console.WriteLine($"Id после добавления в базу данных {user.Id}");  // Id = 1 
 }
 
@@ -19,8 +21,8 @@ using (ApplicationContext db = new())
 using (ApplicationContext db = new())    // На уровне базы данных это будет проявляться в установке
 {                                                           // параметра DEFAULT:
     User user1 = new User() { Name = "Tom"};                // CREATE TABLE "Users" (
-    Console.WriteLine($"Age: {user1.Age}"); // 0      // "Id"    INTEGER NOT NULL,
-                                            // "Name"  TEXT,
+    Console.WriteLine($"Age: {user1.Age}");           // "Id"    INTEGER NOT NULL,
+                                                      // "Name"  TEXT,
     db.Users.Add(user1);                                    // "Age"   INTEGER NOT NULL DEFAULT 18,
     db.SaveChanges();                                       // CONSTRAINT "PK_Users" PRIMARY KEY("Id" AUTOINCREMENT)
                                                             // );
