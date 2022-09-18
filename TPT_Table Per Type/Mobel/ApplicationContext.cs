5a11e8102ed350e478d12f66fbc7a10ac43e84bd
Александр Середин <agames1448@gmail.com>
2022-09-18 16:32:27 +0300

    [Added] database context

---
+++ TPT_Table Per Type/Mobel/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TPT_Table_Per_Type.Mobel
+{
+    internal class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+
+        public DbSet<Employee> Employees { get; set; } = null!;
+
+        public DbSet<Manager> Managers { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=helloapp.db");
+        }
+    }
+}
