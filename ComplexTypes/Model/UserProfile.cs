5bf653a29995870bf9f443ada9868c1b54c6771b
Александр Середин <agames1448@gmail.com>
2022-09-18 14:31:11 +0300

    [Added] Models

---
+++ ComplexTypes/Model/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ComplexTypes.Model
+{
+
+    [Owned]
+    public class UserProfile
+    {
+        public int Id { get; set; }
+
+        public int Age { get; set; }
+
+        public string? Name { get; set; }
+    }
+}
