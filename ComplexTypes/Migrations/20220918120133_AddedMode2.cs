b648fcb123ed7560bd7a084922380b4033e161c8
Александр Середин <agames1448@gmail.com>
2022-09-18 15:04:12 +0300

    [added] migration database

---
+++ ComplexTypes/Migrations/20220918120133_AddedMode2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComplexTypes.Migrations
+{
+    public partial class AddedMode2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Profile_Surname",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Profile_Surname",
+                table: "Users");
+        }
+    }
+}
