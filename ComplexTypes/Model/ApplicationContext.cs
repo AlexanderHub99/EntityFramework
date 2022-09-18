704983c4df8734f1745e2f641d89649e7368e35b
Александр Середин <agames1448@gmail.com>
2022-09-18 14:31:44 +0300

    [Added] ApplicationContext

---
+++ ComplexTypes/Model/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ComplexTypes.Model
+{
+    internal class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+
+        public ApplicationContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = helloapp.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Настройки связи можно использовать Fluent API, в частности, метод OwnsOne()
+            modelBuilder.Entity<User>().OwnsOne(u => u.Profile);
+        }
+    }
+}
